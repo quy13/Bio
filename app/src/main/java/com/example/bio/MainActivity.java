dbc2e136434f1b8bd63dc438de5c4558549cadc6
quy13 <87173367+quy13@users.noreply.github.com>
2021-08-03 20:40:43 +0700

    commit

---
+++ app/src/main/java/com/example/bio/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.bio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Context;
+import android.database.DatabaseUtils;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.bio.data.Bio2;
+import com.example.bio.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private final Bio2 bio = new Bio2();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        bio.setName2("Hua Duy Phu");
+
+        binding.setBio(bio);
+        binding.button.setOnClickListener(this::showhobbies);
+    }
+
+    public void showhobbies(View view) {
+
+        bio.setHobbies2(String.format("Hobbies : %s", binding.EtHobby
+                .getText().toString().trim()));
+        //drop all binding
+        binding.invalidateAll();
+        binding.showText.setVisibility(View.VISIBLE);
+
+        //hide keyboard
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethodManager != null;
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
---
+++ app/src/main/java/com/example/bio/data/Bio2.java
@@ -0,0 +1,31 @@
+package com.example.bio.data;
+
+public class Bio2 {
+    private String name2;
+    private String hobbies2;
+
+    public Bio2(){
+
+    }
+
+    public Bio2(String name2, String hobbies2) {
+        this.name2 = name2;
+        this.hobbies2 = hobbies2;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public String getHobbies2() {
+        return hobbies2;
+    }
+
+    public void setHobbies2(String hobbies2) {
+        this.hobbies2 = hobbies2;
+    }
+}
